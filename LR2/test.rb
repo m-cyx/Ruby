5caa2d3a8859d0fc22847d57850cc7179d263598
m-cyx <markpixel05@gmail.com>
2021-03-29 02:03:46 +0300

    Задание 6_56

--- LR2/test.rb
+++ LR2/test.rb
@@ -1,13 +1,25 @@
-arr = [1.2, 1, 1.3, 2, 1.4, 3]
+arr = gets.chomp.split.map(&:to_i)
 
-def z_44 (arr)
-    for i in 0..arr.length-1
-        if arr[i].class == arr[i+1].class
-            return false
+def z_56 (arr)
+    mean_even = 0
+    cnt = 0
+    cnt2 = 0
+    mean_not_even = 0
+    for el in arr
+        if el.even?
+            mean_even += el
+            cnt += 1
         end
     end
-    return true
+    mean_even = mean_even / cnt
+    for el in arr
+        if el.even? == false and el > mean_even
+            mean_not_even += el
+            cnt2 += 1
+        end
+    end
+    return mean_not_even = mean_not_even / cnt2
 end
 
 
-print(z_44(arr))
+print(z_56(arr))
