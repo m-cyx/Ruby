550848ba052c67561f05e0ed9716966b01edb976
m-cyx <markpixel05@gmail.com>
2021-03-28 20:59:01 +0300

    Пофиксил

--- LR1/pz4.rb
+++ LR1/pz4.rb
@@ -5,10 +5,23 @@
 Включить снимок экрана с результатом в отчет.
 """
 
-puts("Привет, введи комманду руби:")
-system ("irb")   НЕ НАДО
-открыть учебник
+puts "Привет, введи команду ОС:"
+command1 = STDIN.gets.chomp
 
-puts("Привет, введи комманду OS:")
-command1 =gets.chomp
-system (command1)
+puts "Привет, введи команду Ruby:"
+command2 = STDIN.gets.chomp
+
+puts "Команда ОС:"
+system(command1)
+
+puts "Команда Ruby:"
+puts eval(command2)
+
+"""
+eval()
+Evaluates the Ruby expression(s) in string. 
+If binding is given, which must be a Binding object, 
+the evaluation is performed in its context. 
+If the optional filename and lineno parameters are 
+present, they will be used when reporting syntax errors.
+"""
